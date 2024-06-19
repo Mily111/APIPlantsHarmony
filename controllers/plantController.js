f6ab7616bfb6a1cf6af94afe8a6c3a0851d564ac
emilie <emiliemalirat@gmail.com>
2024-06-19 16:45:23 +0200

    add and quantity palnt ok

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -13,25 +13,28 @@ exports.getPlantNames = async (req, res) => {
 
 exports.addPlantSuggestion = async (req, res) => {
   const { plantName, stateExchange, userId } = req.body;
-  const photo = req.file.path;
+  const photo = req.file ? `images/plants/${req.file.filename}` : null;
+
+  console.log("Request body:", req.body);
+  console.log("Uploaded file:", req.file);
+
+  if (!plantName || !stateExchange || !userId) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
 
   try {
-    const result = await plantModel.addPlantSuggestionToDB({
+    await plantModel.addOrUpdatePlantSuggestion({
       plantName,
       stateExchange,
       photo,
       userId,
     });
-    if (result) {
-      res.json({ message: "Plant suggestion added" });
-    } else {
-      res
-        .status(500)
-        .json({ message: "Erreur lors de l'ajout de la plante suggérée" });
-    }
+    res.json({ message: "Plant suggestion added or updated" });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Erreur lors de l'ajout de la plante suggérée" });
+    console.error("Error adding or updating plant suggestion:", error);
+    res.status(500).json({
+      message:
+        "Erreur lors de l'ajout ou de la mise à jour de la suggestion de plante",
+    });
   }
 };
--- routes/plantRoutes.js
+++ routes/plantRoutes.js
@@ -2,7 +2,25 @@ const express = require("express");
 const router = express.Router();
 const plantController = require("../controllers/plantController");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const path = require("path");
+// Définissez le chemin complet vers le dossier public/images/plants de votre application Next.js
+const nextjsPublicPath = path.join(
+  __dirname,
+  "../../PLANTS-HARMONY-WEB/public/images/plants"
+);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, nextjsPublicPath);
+  },
+  filename: function (req, file, cb) {
+    const plantName = req.body.plantName.replace(/\s+/g, "_").toLowerCase(); // Remplacer les espaces par des underscores et convertir en minuscule
+    const extension = path.extname(file.originalname);
+    cb(null, `${plantName}${extension}`);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/plantsName", plantController.getPlantNames);
 router.post(
@@ -10,11 +28,5 @@ router.post(
   upload.single("photo"),
   plantController.addPlantSuggestion
 );
-
-// router.get("/", plantController.getAllPlants);
-// router.post("/", plantController.createPlant);
-// router.get("/:id", plantController.getPlantById);
-// router.put("/:id", plantController.updatePlant);
-// router.delete("/:id", plantController.deletePlant);
 
 module.exports = router;
